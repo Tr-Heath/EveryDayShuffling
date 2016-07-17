c970dc331f9984b4eca1ffd407bbd6fc1ca0bd50
Tr-Heath <travis.heath@outlook.com>
2016-07-17 16:22:48 -0700

    Add project files.

---
+++ TestShuffle/Core.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestShuffle
+{
+    [TestClass]
+    public class Core
+    {
+        [TestMethod]
+        public void SortDeck_Sorted()
+        {
+        }
+        [TestMethod]
+        public void ShuffleDeck_Shuffled()
+        {
+        }
+        [TestMethod]
+        public void ShuffleDeck_NotBiased()
+        {
+        }
+        [TestMethod]
+        public void CreateDeck_Valid52CardDeck()
+        {
+        }
+    }
+}
